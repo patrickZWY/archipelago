525618fcaca655cdb180004c50b8e1279c355b96
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2025-02-15 17:00:53 -0500

    mybatis config

--- src/main/java/com/archipelago/config/MyBatisConfig.java
+++ src/main/java/com/archipelago/config/MyBatisConfig.java
@@ -1,4 +1,19 @@
 package com.archipelago.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        return factoryBean.getObject();
+    }
 }
