8c38acab7b3bb11d67c04174dfb098d522e1f4ad
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2025-02-24 21:57:43 -0500

    fix pom

--- src/main/java/com/archipelago/ArchipelagoApplication.java
+++ src/main/java/com/archipelago/ArchipelagoApplication.java
@@ -1,9 +1,11 @@
 package com.archipelago;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.archipelago.mapper")
 public class ArchipelagoApplication {
 
 	public static void main(String[] args) {
